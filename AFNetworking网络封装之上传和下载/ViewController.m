b2a678665c252a4fec8605475a09e7eb51542f30
Mr.Lizhen <alizhen_work@163.com>
2016-02-14 15:36:13 +0800

    first commit

--- AFNetworking网络封装之上传和下载/ViewController.m
+++ AFNetworking网络封装之上传和下载/ViewController.m
@@ -16,7 +16,42 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+#warning 上传
+    
+//    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+//    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+//    
+//    NSURL *URL = [NSURL URLWithString:@"http://example.com/upload"];
+//    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+//    
+//    NSURL *filePath = [NSURL fileURLWithPath:@"file://path/to/image.png"];
+//    NSURLSessionUploadTask *uploadTask = [manager uploadTaskWithRequest:request fromFile:filePath progress:nil completionHandler:^(NSURLResponse *response, id responseObject, NSError *error) {
+//        if (error) {
+//            NSLog(@"Error: %@", error);
+//        } else {
+//            NSLog(@"Success: %@ %@", response, responseObject);
+//        }
+//    }];
+//    [uploadTask resume];
+    
+#warning 下载
+    
+//    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+//    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+//    
+//    NSURL *URL = [NSURL URLWithString:@"http://example.com/download.zip"];
+//    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+//    
+//    NSURLSessionDownloadTask *downloadTask = [manager downloadTaskWithRequest:request progress:nil destination:^NSURL *(NSURL *targetPath, NSURLResponse *response) {
+//        NSURL *documentsDirectoryURL = [[NSFileManager defaultManager] URLForDirectory:NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:NO error:nil];
+//        return [documentsDirectoryURL URLByAppendingPathComponent:[response suggestedFilename]];
+//    } completionHandler:^(NSURLResponse *response, NSURL *filePath, NSError *error) {
+//        NSLog(@"File downloaded to: %@", filePath);
+//    }];
+//    //重新开始下载
+//    [downloadTask resume];
 }
 
 - (void)didReceiveMemoryWarning {
